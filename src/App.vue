bf90ed57648046d11a7bc655b8a882be905af163
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-06-09 12:38:38 +0200

    adding a slot so that the parent can decide the markup and design of list items

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,72 @@
 <script setup>
+import {onMounted, ref} from "vue";
+import UserComponent from "@/components/UserComponent.vue";
+
+const itemRefs = ref([]);
+const heightStyle = ref(0);
+
+const list = ref([
+  {id: 1, name: 'John Doe 1', age: 50, country: 'Spain'},
+  {id: 2, name: 'Jane Doe 2', age: 30, country: 'Senegal'},
+  {id: 3, name: 'Smith John 3', age: 54, country: 'Morocco'},
+  {id: 4, name: 'John Doe 4', age: 50, country: 'Spain'},
+  {id: 5, name: 'Jane Doe 5', age: 30, country: 'Senegal'},
+  {id: 6, name: 'Smith John 6', age: 54, country: 'Morocco'},
+  {id: 7, name: 'John Doe 7', age: 50, country: 'Spain'},
+  {id: 8, name: 'Jane Doe 8', age: 30, country: 'Senegal'},
+  {id: 9, name: 'Smith John 9', age: 54, country: 'Morocco'},
+  {id: 10, name: 'John Doe 10', age: 50, country: 'Spain'},
+  {id: 11, name: 'Jane Doe 11', age: 30, country: 'Senegal'},
+  {id: 12, name: 'Smith John 12', age: 54, country: 'Morocco'},
+]);
+
+
 
 </script>
-
 <template>
-
+  <div class="viewport-center">
+    <user-component :list="list">
+      <template #item="user">
+        <img class="avatar" :src="'/placeholder-avatar.jpg'" alt=""/>
+        <div>
+          <p class="mb-2">{{ user.name }}</p>
+          <button>Vote {{ user.votes }}</button>
+        </div>
+      </template>
+    </user-component>
+  </div>
 </template>
 
-<style scoped>
+<style>
+.root-card {
+  @apply flex flex-wrap justify-center items-center
+}
 
+.user-wrapper {
+  @apply gap-10 justify-center;
+}
+
+.user-card {
+  @apply p-3 shadow-lg rounded w-80 relative flex items-center;
+}
+
+.avatar {
+  @apply font-bold object-cover w-24 h-24 rounded-full mr-5 dark:bg-slate-700 bg-slate-200 inline-flex justify-center items-center text-3xl;
+}
+
+button {
+  @apply bg-blue-600 rounded-md border border-blue-800 px-2  text-white;
+}
+
+.pagination {
+  @apply flex gap-2 justify-center mt-10;
+}
+
+.pagination button {
+  @apply bg-blue-600 rounded-md border border-blue-800 p-3 px-6 text-lg text-white;
+}
+
+.pagination .active button {
+  @apply bg-blue-900;
+}
 </style>
---
+++ src/components/UserComponent.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps({
+  total: {default: 3, type: Number},
+  current: { default: 1, type: Number},
+  list: { type: Array, default: ()=>[]}
+})
+</script>
+<template>
+  <div class="root-card m-5 w-6/12 mx-auto border border-black h-80 overflow-hidden" >
+    <ul class="user-wrapper">
+      <li
+          ref="itemRefs"
+          class="user-card"
+          v-for="(user, index) in list"
+          :key="user.id"
+          :tabindex="index + 1"
+      >
+       <slot name="item" v-bind="user"> </slot>
+      </li>
+    </ul>
+  </div>
+  <div>
+    <ul class="pagination">
+      <li
+          v-for="n in total"
+          :key="n"
+      >
+        <button>{{ n }}</button>
+      </li>
+    </ul>
+  </div>
+</template>
+<style scoped>
+
+</style>
